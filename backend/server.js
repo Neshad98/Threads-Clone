f7a4dd98adcd7371a048f2b8024eba5b087a1c2b
Neshad <djneshad@gmail.com>
2024-02-05 12:49:54 +0600

    backend working

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,24 @@
 import express from "express";
 import dotenv from 'dotenv';
 import connectDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
+import cors from 'cors';
 
 dotenv.config();
 connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
+
+
+//Routes
+app.use("/api/users", userRoutes);
+
 
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
 
