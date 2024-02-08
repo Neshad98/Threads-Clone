e2169cc6c1a45e0c8e70eb0dc1b54e63f119ef9b
Neshad <djneshad@gmail.com>
2024-02-08 14:28:14 +0600

    backend

---
+++ backend/controllers/userController.js
@@ -0,0 +1,17 @@
+import User from "../models/userModel";
+
+const signupUser = async (req, res) => {
+  try {
+    const { name, email, username, password } = req.body;
+    const user = await User.findOne({ $or: [{ email }, { username }] });
+
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+    console.log("Error in signupUser: ", err.message);
+  }
+}
+
+export { signupUser };
