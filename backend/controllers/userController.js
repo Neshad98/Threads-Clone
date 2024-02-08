6bd374cd080bd44ff9f2b5cd65d6f2c980dc1966
Neshad <djneshad@gmail.com>
2024-02-08 20:17:53 +0600

    backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,7 @@
-import User from "../models/userModel";
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/helpers/generateTokenAndSetCookie.js";
+// import mongoose from "mongoose";
 
 const signupUser = async (req, res) => {
   try {
@@ -8,6 +11,30 @@ const signupUser = async (req, res) => {
     if (user) {
       return res.status(400).json({ message: "User already exists" });
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      name,
+      email,
+      username,
+      password: hashedPassword
+    });
+    await newUser.save();
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      res.status(201).json({
+        _id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        username: newUser.username,
+      })
+    } else {
+      res.status(400).json({ message: "Invalid user data" })
+    }
+
   } catch (err) {
     res.status(500).json({ message: err.message });
     console.log("Error in signupUser: ", err.message);
