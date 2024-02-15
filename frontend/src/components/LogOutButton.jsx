ea37b3372dd76aee2e17440a97148e28a98a3671
Neshad <djneshad@gmail.com>
2024-02-15 20:38:11 +0600

    update profile

--- frontend/src/components/LogOutButton.jsx
+++ frontend/src/components/LogOutButton.jsx
@@ -2,6 +2,7 @@ import { Button } from "@chakra-ui/react"
 import { useSetRecoilState } from "recoil";
 import userAtom from "../atoms/userAtom";
 import useShowToast from "../hooks/useShowToast";
+import { FiLogOut } from "react-icons/fi";
 
 
 const LogOutButton = () => {
@@ -32,7 +33,7 @@ const LogOutButton = () => {
   return (
     <Button
       position={"fixed"} top={"30px"} right={"30px"} size={"sm"} onClick={handleLogout}
-    >Logout
+    ><FiLogOut size={20} />
     </Button>
 
   )
