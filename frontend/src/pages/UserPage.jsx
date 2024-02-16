458dbd05e8ffadf6fc60a0c8b7c82efbbe9a155b
Neshad <djneshad@gmail.com>
2024-02-16 19:51:35 +0600

    follow unfollow working

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react"
 import UserHeader from "../components/UserHeader"
 import UserPost from "../components/UserPost"
+import { useParams } from "react-router-dom";
+import useShowToast from "../hooks/useShowToast";
 
 
 const UserPage = () => {
+  const [user, setUser] = useState(null);
+  const { username } = useParams();
+  const showToast = useShowToast();
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const res = await fetch(`/api/users/profile/${username}`)
+        const data = await res.json();
+        if (data.error) {
+          showToast("Error", data.error, "error");
+          return;
+        }
+        setUser(data);
+        console.log(data);
+      } catch (error) {
+        showToast("Error", error, "error");
+      }
+    }
+    getUser();
+  }, [username, showToast])
+
+  if (!user) return null;
+
   return <>
-    <UserHeader />
+    <UserHeader user={user} />
     <UserPost likes={1200} replies={269} postImg="/post1.png" postTitle="Let's talk about threads." />
     <UserPost likes={1900} replies={520} postImg="/post2.png" postTitle="Niceee tutorial" />
     <UserPost likes={700} replies={370} postImg="/post3.png" postTitle="I love this guy" />
