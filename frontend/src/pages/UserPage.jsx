ac3766ee5339f9078dce626324fc5619290cbe11
Neshad <djneshad@gmail.com>
2024-02-18 23:15:34 +0600

    workinggg

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -3,12 +3,14 @@ import UserHeader from "../components/UserHeader"
 import UserPost from "../components/UserPost"
 import { useParams } from "react-router-dom";
 import useShowToast from "../hooks/useShowToast";
+import { Spinner } from "@chakra-ui/react";
 
 
 const UserPage = () => {
   const [user, setUser] = useState(null);
   const { username } = useParams();
   const showToast = useShowToast();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getUser = async () => {
@@ -23,12 +25,20 @@ const UserPage = () => {
         console.log(data);
       } catch (error) {
         showToast("Error", error, "error");
+      } finally {
+        setLoading(false);
       }
     }
     getUser();
   }, [username, showToast])
 
-  if (!user) return null;
+
+  if (!user && loading) {
+    return (
+      <Spinner size={"xl"} />
+    )
+  }
+  if (!user && !loading) return <h1>User not found.</h1>
 
   return <>
     <UserHeader user={user} />
