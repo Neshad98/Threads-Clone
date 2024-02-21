472e90df098898b5cf34db6c3b4b314066eb05d0
Neshad <djneshad@gmail.com>
2024-02-21 21:09:32 +0600

    working

--- frontend/src/pages/UpdateProfilePage.jsx
+++ frontend/src/pages/UpdateProfilePage.jsx
@@ -19,6 +19,8 @@ import useShowToast from '../hooks/useShowToast';
 
 export default function UpdateProfilePage() {
   const [user, setUser] = useRecoilState(userAtom);
+  //state for closing the fckn form 
+  const [formOpen, setFormOpen] = useState(true);
   const [inputs, setInputs] = useState({
     name: user.name,
     username: user.username,
@@ -30,6 +32,9 @@ export default function UpdateProfilePage() {
   const [updating, setUpdating] = useState(false);
   const showToast = useShowToast();
   const { handleImageChange, imgUrl } = usePreviewImg();
+  const handleClose = () => {
+    setFormOpen(false);
+  }
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (updating) return;
@@ -57,110 +62,114 @@ export default function UpdateProfilePage() {
     }
   }
   return (
-    <form onSubmit={handleSubmit}>
-      <Flex
-        align={'center'}
-        justify={'center'} my={6}
-      >
-        <Stack
-          spacing={4}
-          w={'full'}
-          maxW={'md'}
-          bg={useColorModeValue('white', 'gray.dark')}
-          rounded={'xl'}
-          boxShadow={'lg'}
-          p={6}
-        >
-          <Heading lineHeight={1.1} fontSize={{ base: '2xl', sm: '3xl' }}>
-            User Profile Edit
-          </Heading>
-          <FormControl id="userName">
-            <Stack direction={['column', 'row']} spacing={6}>
-              <Center>
-                <Avatar size="xl" boxShadow={"md"} src={imgUrl || user.profilePic} />
-
-              </Center>
-              <Center w="full">
-                <Button w="full" onClick={() => fileRef.current.click()}
-                >Change Avatar</Button>
-                <Input type='file' hidden ref={fileRef} onChange={handleImageChange} />
-              </Center>
-            </Stack>
-          </FormControl>
-          <FormControl>
-            <FormLabel>Full name</FormLabel>
-            <Input
-              value={inputs.name}
-              onChange={(e) => setInputs({ ...inputs, name: e.target.value })}
-              placeholder="Full Name"
-              _placeholder={{ color: 'gray.500' }}
-              type="text"
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel>User name</FormLabel>
-            <Input
-              value={inputs.username}
-              onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
-              placeholder="UserName"
-              _placeholder={{ color: 'gray.500' }}
-              type="text"
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel>Email address</FormLabel>
-            <Input
-              value={inputs.email}
-              onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
-              placeholder="your-email@example.com"
-              _placeholder={{ color: 'gray.500' }}
-              type="email"
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel>Bio</FormLabel>
-            <Input
-              value={inputs.bio}
-              onChange={(e) => setInputs({ ...inputs, bio: e.target.value })}
-              placeholder="your bio"
-              _placeholder={{ color: 'gray.500' }}
-              type="text"
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel>Password</FormLabel>
-            <Input
-              value={inputs.password}
-              onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
-              placeholder="password"
-              _placeholder={{ color: 'gray.500' }}
-              type="password"
-            />
-          </FormControl>
-          <Stack spacing={6} direction={['column', 'row']}>
-            <Button
-              bg={'red.400'}
-              color={'white'}
-              w="full"
-              _hover={{
-                bg: 'red.500',
-              }}>
-              Cancel
-            </Button>
-            <Button
-              bg={'green.400'}
-              color={'white'}
-              w="full"
-              _hover={{
-                bg: 'blue.500',
-              }} type='submit'
-              isLoading={updating}
+    <>
+      {formOpen && (
+        <form onSubmit={handleSubmit}>
+          <Flex
+            align={'center'}
+            justify={'center'} my={6}
+          >
+            <Stack
+              spacing={4}
+              w={'full'}
+              maxW={'md'}
+              bg={useColorModeValue('white', 'gray.dark')}
+              rounded={'xl'}
+              boxShadow={'lg'}
+              p={6}
             >
-              Submit
-            </Button>
-          </Stack>
-        </Stack>
-      </Flex>
-    </form>
+              <Heading lineHeight={1.1} fontSize={{ base: '2xl', sm: '3xl' }}>
+                User Profile Edit
+              </Heading>
+              <FormControl id="userName">
+                <Stack direction={['column', 'row']} spacing={6}>
+                  <Center>
+                    <Avatar size="xl" boxShadow={"md"} src={imgUrl || user.profilePic} />
+
+                  </Center>
+                  <Center w="full">
+                    <Button w="full" onClick={() => fileRef.current.click()}
+                    >Change Avatar</Button>
+                    <Input type='file' hidden ref={fileRef} onChange={handleImageChange} />
+                  </Center>
+                </Stack>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Full name</FormLabel>
+                <Input
+                  value={inputs.name}
+                  onChange={(e) => setInputs({ ...inputs, name: e.target.value })}
+                  placeholder="Full Name"
+                  _placeholder={{ color: 'gray.500' }}
+                  type="text"
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>User name</FormLabel>
+                <Input
+                  value={inputs.username}
+                  onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
+                  placeholder="UserName"
+                  _placeholder={{ color: 'gray.500' }}
+                  type="text"
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Email address</FormLabel>
+                <Input
+                  value={inputs.email}
+                  onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
+                  placeholder="your-email@example.com"
+                  _placeholder={{ color: 'gray.500' }}
+                  type="email"
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Bio</FormLabel>
+                <Input
+                  value={inputs.bio}
+                  onChange={(e) => setInputs({ ...inputs, bio: e.target.value })}
+                  placeholder="your bio"
+                  _placeholder={{ color: 'gray.500' }}
+                  type="text"
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Password</FormLabel>
+                <Input
+                  value={inputs.password}
+                  onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
+                  placeholder="password"
+                  _placeholder={{ color: 'gray.500' }}
+                  type="password"
+                />
+              </FormControl>
+              <Stack spacing={6} direction={['column', 'row']}>
+                <Button
+                  bg={'red.400'}
+                  color={'white'}
+                  w="full"
+                  _hover={{
+                    bg: 'red.500',
+                  }} onClick={handleClose} >
+                  Cancel
+                </Button>
+                <Button
+                  bg={'green.400'}
+                  color={'white'}
+                  w="full"
+                  _hover={{
+                    bg: 'blue.500',
+                  }} type='submit'
+                  isLoading={updating}
+                >
+                  Submit
+                </Button>
+              </Stack>
+            </Stack>
+          </Flex>
+        </form>
+      )}
+    </>
   )
 }
