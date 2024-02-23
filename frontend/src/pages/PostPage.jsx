6f93759ff3c0d52b4e4f9ee5f2c83c2e405b7e12
Neshad <djneshad@gmail.com>
2024-02-23 12:35:56 +0600

    working to finish it

--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -1,18 +1,44 @@
-import { Avatar, Box, Button, Divider, Flex, Image, Text } from "@chakra-ui/react"
+import { Avatar, Box, Button, Divider, Flex, Image, Spinner, Text } from "@chakra-ui/react"
 import { BsThreeDots } from "react-icons/bs"
 import Actions from "../components/Actions"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Comment from "../components/Comment"
+import useGetUserProfile from "../hooks/useGetUserProfile"
+import useShowToast from "../hooks/useShowToast"
+import { useParams } from "react-router-dom"
 
 
 const PostPage = () => {
-  const [liked, setLiked] = useState(false);
+  const { user, loading } = useGetUserProfile();
+  const [post, setPosts] = useState(null);
+  const showToast = useShowToast();
+  const { pid } = useParams();
+
+  useEffect(() => {
+    const getPost = async () => {
+      try {
+
+      } catch (error) {
+        showToast("Error", error.message, "error")
+      }
+    }
+    getPost();
+  }, [])
+
+  if (!user && loading) {
+    return (
+      <Flex justifyContent={"center"}>
+        <Spinner size={"xl"} />
+      </Flex>
+    )
+  }
+
   return <>
     <Flex>
       <Flex w={"full"} alignItems={"center"} gap={3}>
-        <Avatar src="/zuck-avatar.png" size={"md"} name="Mark Zuckerberg" />
+        <Avatar src={user.profilePic} size={"md"} name="username" />
         <Flex>
-          <Text fontSize={"small"} fontWeight={"bold"}>markzuckerberg</Text>
+          <Text fontSize={"small"} fontWeight={"bold"}>{user.username}</Text>
           <Image src="/verified.png" w={4} h={4} ml={4} />
         </Flex>
       </Flex>
@@ -28,14 +54,14 @@ const PostPage = () => {
     </Box>
 
     <Flex gap={3} my={3}>
-      <Actions liked={liked} setLiked={setLiked} />
+      <Actions post={post} />
     </Flex>
 
     <Flex gap={2} alignItems={"center"}>
       <Text color={"gray.light"} fontSize={"small"}>238 replies</Text>
       <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
       <Text color={"gray.light"} fontSize={"small"}>
-        {200 + (liked ? 1 : 0)} likes
+        {200} likes
       </Text>
     </Flex>
     <Divider my={4} />
@@ -51,15 +77,10 @@ const PostPage = () => {
     </Flex>
 
     <Divider my={4} />
-    <Comment
+    {/* <Comment
       comment="Looks really good!" createdAt="2d" likes={100} username="John doe"
-      userAvatar="https://bit.ly/dan-abramov" />
-    <Comment
-      comment="Looks really good!" createdAt="2d" likes={150} username="Sally doe"
-      userAvatar="https://bit.ly/dan-abramov" />
-    <Comment
-      comment="Looks really good!" createdAt="2d" likes={400} username="Mei doe"
-      userAvatar="https://bit.ly/dan-abramov" />
+      userAvatar="https://bit.ly/dan-abramov" /> */}
+
 
   </>
 
